f33d3468ca88a38582d6041a5312f75157f858b7
LukWarm <egandonley@gmail.com>
2019-10-23 02:57:45 -0400

    setup auth URI

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+const request = require('request');
+
 
 var app = express();
 
@@ -7,8 +9,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res) {
-  console.log(req.query);
-  res.send('Secret area');
+  const code = req.query.code;
+  console.log(code);
+
+  request({
+    method: 'POST',
+    url: `https://api.clickup.com/api/v2/oauth/token?client_id=Q9JXVN7585QPRWZLSU25PBN2NZSPIZBV&client_secret=TQ984136S6GWSDETIAFDOARPXREBJLPOKOR74QXB6FTS6UI0RSLGU7U7RG4DA34U&code=${code}`,
+  }, function (error, response, body) {
+    console.log('Status:', response.statusCode);
+    console.log('Headers:', JSON.stringify(response.headers));
+    console.log('Response:', body);
+    res.send(body.access_token);
+  });
 });
 
 app.listen(3000, () => {
