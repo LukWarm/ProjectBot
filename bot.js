8ac0c2420211d33b4c2787f1961c570175c18b0a
LukWarm <egandonley@gmail.com>
2019-10-24 01:41:06 -0400

    remove debugging, add comments

--- bot.js
+++ bot.js
@@ -11,15 +11,22 @@ const prefix = '!';
 bot.on('ready', () => {
   console.log('Bot is Online!');
 })
+// wait until bot is ready to start accepting commands
 
 bot.on('message', message => {
   let args = message.content.substring(prefix.length).split(" ");
+  // split message that contains prefix into arguments by spacing,
+  // and putting them into an array
   switch (args[0]) {
     case ('ping'):
+      // basic connection test
       message.channel.send('Pong!');
       break;
     case ('spaces'):
+      // find and print spaces
+      // usage: !spaces (blank : archived)
       if (!args[1]) {
+        // if no second argument (not archived)
         request({
           method: 'GET',
           url: `${api}team/${team_id}/space?archived=false`,
@@ -27,20 +34,21 @@ bot.on('message', message => {
             'Authorization': clickToken
           }}, function (error, response, body) {
             body = JSON.parse(body);
-            console.log('Status:', response.statusCode);
-            console.log('Response:', body);
             if (!body.spaces[0]) {
               message.channel.send('There are currently no active spaces.');
+              // if no items in array, no active spaces
             } else {
               let msg = '';
               for ( i in body.spaces ) {
                 i === 0 ? msg = `> ${body.spaces[i].name}\n` : msg = msg + `> ${body.spaces[i].name}\n`;
-                console.log(msg);
+                // for each item in array,
+                // add to message with blockqoute and line break
               }
               message.channel.send(`Active Spaces: \n${msg}`);
             }
           });
       } else if (args[1] === 'archived') {
+        // if looking for archived spaces
         request({
           method: 'GET',
           url: `${api}team/${team_id}/space?archived=true`,
@@ -48,15 +56,15 @@ bot.on('message', message => {
             'Authorization': clickToken
           }}, function (error, response, body) {
             body = JSON.parse(body);
-            console.log('Status:', response.statusCode);
-            console.log('Response:', body);
             if (!body.spaces[0]) {
               message.channel.send('There are currently no archived spaces.');
+              // if no items in array, no archived spaces
             } else {
               let msg = '';
               for ( i in body.spaces ) {
                 i === 0 ? msg = `> ${body.spaces[i].name}\n` : msg = msg + `> ${body.spaces[i].name}\n`;
-                console.log(msg);
+                // for each item in array,
+                // add to message with blockqoute and line break
               }
               message.channel.send(`Archived Spaces: \n${msg}`);
             }
@@ -66,6 +74,7 @@ bot.on('message', message => {
       }
       break;
     case ('teams'):
+      // print what team the bot is in (should be CircuitRunners all the time)
       request({
         method: 'GET',
         url: `${api}team`,
@@ -73,12 +82,14 @@ bot.on('message', message => {
           'Authorization': clickToken
         }}, function (error, response, body) {
           body = JSON.parse(body);
-          console.log('Status:', response.statusCode);
-          console.log('Response: ', body);
           message.channel.send(`You are in the ${body.teams[0].name} team!`);
         });
+      break;
+    case ('folders'):
+      // find and print folders for a specific space
       break;
   }
 })
 
 bot.login(discordToken);
+// login to discord using bot token
